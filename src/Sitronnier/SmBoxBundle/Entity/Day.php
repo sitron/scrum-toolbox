73ff23a5b6ccecd5d7b984214c19f05e253c9884
Laurent Prodon <laurent@sitronnier.com>
2011-10-22 22:35:00 +0200

    added day entity

---
+++ src/Sitronnier/SmBoxBundle/Entity/Day.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Sitronnier\SmBoxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sitronnier\SmBoxBundle\Entity\Day
+ */
+class Day
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var float $nb hours
+     */
+    private $nb_hours;
+
+    /**
+     * @var float $nb business value
+     */
+    private $nb_business_value;
+
+    /**
+     * @var float $nb story points
+     */
+    private $nb_story_points;
+
+    /**
+     * @var Sprint $sprint id
+     */
+    private $sprint;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nb hours
+     *
+     * @param float $nbHours
+     */
+    public function setNbHours($nbHours)
+    {
+        $this->nb_hours = $nbHours;
+    }
+
+    /**
+     * Get nb hours
+     *
+     * @return float 
+     */
+    public function getNbHours()
+    {
+        return $this->nb_hours;
+    }
+
+    /**
+     * Set nb business value
+     *
+     * @param float $nbBusinessValue
+     */
+    public function setNbBusinessValue($nbBusinessValue)
+    {
+        $this->nb_business_value = $nbBusinessValue;
+    }
+
+    /**
+     * Get nb business value
+     *
+     * @return float 
+     */
+    public function getNbBusinessValue()
+    {
+        return $this->nb_business_value;
+    }
+
+    /**
+     * Set nb story points
+     *
+     * @param float $nbStoryPoints
+     */
+    public function setNbStoryPoints($nbStoryPoints)
+    {
+        $this->nb_story_points = $nbStoryPoints;
+    }
+
+    /**
+     * Get nb story points
+     *
+     * @return float 
+     */
+    public function getNbStoryPoints()
+    {
+        return $this->nb_story_points;
+    }
+
+    /**
+     * Set sprint id
+     *
+     * @param object $sprintId
+     */
+    public function setSprint($sprint)
+    {
+        $this->sprint = $sprint;
+    }
+
+    /**
+     * Get sprint id
+     *
+     * @return object 
+     */
+    public function getSprint()
+    {
+        return $this->sprint;
+    }
+}
+
---
+++ src/Sitronnier/SmBoxBundle/Form/DayType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sitronnier\SmBoxBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class DayType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('nb_hours')
+            ->add('nb_business_value')
+            ->add('nb_story_points')
+            ->add('sprint')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'sitronnier_smboxbundle_daytype';
+    }
+}
