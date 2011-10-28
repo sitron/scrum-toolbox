46a286c6e5a2dc7f6fe907288b0fa8a31884667e
Laurent Prodon <laurent@sitronnier.com>
2011-10-28 22:51:19 +0200

    tests with easel js

--- src/Sitronnier/SmBoxBundle/Resources/public/js/smgraph.js
+++ src/Sitronnier/SmBoxBundle/Resources/public/js/smgraph.js
@@ -1,16 +1,18 @@
 SmGraph = function() {
-    var canvas, stage, bg;
+    var canvas, stage, bg, container;
 
     function draw() {
         canvas = document.getElementById("graph-canvas");
         stage = new Stage(canvas);
+        container = new Container();
         bg = new Shape();
-        stage.addChild(bg);
+        stage.addChild(container);
+        container.addChild(bg);
 
 	    bg.graphics.beginStroke("#ff0000")
-            .setStrokeStyle(2, 1)
-	    	.moveTo(0, 0)
-	    	.lineTo(20, 30)
+            .setStrokeStyle(1, 1)
+	    	.moveTo(0.5, 0.5)
+	    	.lineTo(20.5, 30.5)
 	    	.lineTo(50, 40)
 	    	.lineTo(120, 50)
 	    	.endStroke()
