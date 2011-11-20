ec1dfba0dd8a819c4599d50fb2efd2d93ec661b3
Laurent Prodon <laurent@sitronnier.com>
2011-11-20 21:32:38 +0100

    make gjslint happy

--- src/Sitronnier/SmBoxBundle/Resources/public/js/smgraph4.js
+++ src/Sitronnier/SmBoxBundle/Resources/public/js/smgraph4.js
@@ -1,4 +1,4 @@
-YUI.add('SprintChart', function (Y) {
+YUI.add('SprintChart', function(Y) {
     Y.namespace('SmbSprintChart');
 
     var chart, chartValues, shownSeries, axes, seriesCollection;
@@ -10,42 +10,42 @@ YUI.add('SprintChart', function (Y) {
 
     // define axes
     axes = {
-        md:{
-            keys:["MD"],
-            position:"left",
-            type:"numeric",
+        md: {
+            keys: ['MD'],
+            position: 'left',
+            type: 'numeric',
             minimum: 0,
             maximum: 9
         },
-        bv:{
-            keys:["BV"],
-            position:"right",
-            type:"numeric",
+        bv: {
+            keys: ['BV'],
+            position: 'right',
+            type: 'numeric',
             minimum: 0,
             maximum: 50
         },
-        sp:{
-            keys:["SP"],
-            position:"right",
-            type:"numeric",
+        sp: {
+            keys: ['SP'],
+            position: 'right',
+            type: 'numeric',
             minimum: 0,
             maximum: 20
         },
-        dates:{
-            keys:["date"],
-            position:"bottom",
-            type:'category',
+        dates: {
+            keys: ['date'],
+            position: 'bottom',
+            type: 'category',
 //            type:"time",
 //            labelFormat: "%a %b %d",
 //            minimum: '11/06/2011',
 //            maximum: '11/20/2011',
-            styles:{
-                majorTicks:{
-                    display: "none"
+            styles: {
+                majorTicks: {
+                    display: 'none'
                 },
                 label: {
-                    rotation:-45,
-                    margin:{top:5}
+                    rotation: -45,
+                    margin: {top: 5}
                 }
             }
         }
@@ -54,31 +54,31 @@ YUI.add('SprintChart', function (Y) {
     // defines series (graphs)
     seriesCollection = [
          {
-            type:"combo",
-            xAxis:"dates",
-            yAxis:"md",
-            xKey:"date",
-            yKey:"MD",
-            xDisplayName:"Date",
-            yDisplayName:"Man/Days",
+            type: 'combo',
+            xAxis: 'dates',
+            yAxis: 'md',
+            xKey: 'date',
+            yKey: 'MD',
+            xDisplayName: 'Date',
+            yDisplayName: 'Man/Days'
         },
         {
-            type:"combo",
-            xAxis:"dates",
-            yAxis:"bv",
-            xKey:"date",
-            yKey:"BV",
-            xDisplayName:"Date",
-            yDisplayName:"Business Value",
+            type: 'combo',
+            xAxis: 'dates',
+            yAxis: 'bv',
+            xKey: 'date',
+            yKey: 'BV',
+            xDisplayName: 'Date',
+            yDisplayName: 'Business Value'
         },
         {
-            type:"combo",
-            xAxis:"dates",
-            yAxis:"sp",
-            xKey:"date",
-            yKey:"SP",
-            xDisplayName:"Date",
-            yDisplayName:"Story Points",
+            type: 'combo',
+            xAxis: 'dates',
+            yAxis: 'sp',
+            xKey: 'date',
+            yKey: 'SP',
+            xDisplayName: 'Date',
+            yDisplayName: 'Story Points'
         }
     ];
 
@@ -96,7 +96,7 @@ YUI.add('SprintChart', function (Y) {
         // Instantiate and render the chart
         chart = new Y.Chart({
             dataProvider: fillDates(chartValues),
-            render: "#graph-canvas",
+            render: '#graph-canvas',
             categoryKey: 'date',
             seriesKeys: shownSeries,
             axes: axes,
@@ -122,7 +122,7 @@ YUI.add('SprintChart', function (Y) {
 
     // update data with ajax call result
     function defineDataProvider(result) {
-        var data = [{'date':'', 'MD':0, 'BV':0, 'SP':0}];
+        var data = [{'date': '', 'MD': 0, 'BV': 0, 'SP': 0}];
         var cSP = 0;
         var cBV = 0;
         var cMD = 0;
@@ -130,7 +130,7 @@ YUI.add('SprintChart', function (Y) {
             cSP += day.nbSP;
             cBV += day.nbBV;
             cMD += day.nbHours;
-            data.push({'date':day.date, 'MD':cMD/8, 'BV':cBV, 'SP':cSP});
+            data.push({'date': day.date, 'MD': cMD / 8, 'BV': cBV, 'SP': cSP});
         });
         return data;
     };
@@ -138,7 +138,7 @@ YUI.add('SprintChart', function (Y) {
     // show at least 10 dates
     var fillDates = function(dates) {
         while (dates.length < 10) {
-            dates.push({'date':'', 'MD':'', 'BV':'', 'SP':''});
+            dates.push({'date': '', 'MD': '', 'BV': '', 'SP': ''});
         }
         return dates;
     }
