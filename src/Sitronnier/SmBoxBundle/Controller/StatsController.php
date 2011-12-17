241846c5114dbc993b32cd56b068aac072b19b80
Laurent Prodon <laurent@sitronnier.com>
2011-12-17 14:29:54 +0100

    removed some unused stuff

--- src/Sitronnier/SmBoxBundle/Controller/StatsController.php
+++ src/Sitronnier/SmBoxBundle/Controller/StatsController.php
@@ -16,28 +16,6 @@ use Sitronnier\SmBoxBundle\Entity\Day;
 class StatsController extends Controller
 {
     /**
-     * Project page
-     */
-    public function projectAction($project_id) {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $securityContext = $this->get('security.context');
-        $user = $securityContext->getToken()->getUser();
-
-        $project = $em->getRepository('SitronnierSmBoxBundle:Project')->findOneBy(array('id' => $project_id, 'owner' => $user->getId()));
-        if (is_null($project)) {
-            throw new AccessDeniedException();
-        }
-        $sprints = $this->getDoctrine()->getRepository('SitronnierSmBoxBundle:Sprint')->findAllForProject($project);
-
-        return $this->render('SitronnierSmBoxBundle:Stats:project.html.twig', array(
-            'project' => $project,
-            'sprints' => $sprints,
-            'lastSprint' => $sprints[count($sprints) - 1],
-        ));
-    }
-
-    /**
      * Sprint graph include
      */
     public function sprintAction($sprint_id) {
