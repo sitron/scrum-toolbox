ea81eca7b2a4600d3b50c9bae38c687d989b4318
Laurent Prodon <laurent@sitronnier.com>
2012-01-09 22:58:38 +0100

    improved deployment script

--- deployment.sh
+++ deployment.sh
@@ -21,29 +21,29 @@ GIT_URL="ssh://git@sitronnier.repositoryhosting.com/sitronnier/sm-box.git"
 TAR_NAME=$TAG_NAME.tgz
 LOCAL_TAR_FILE=$TAR_DIR/$TAR_NAME
 # SERVER_TRANSFER_DIR is a directory relative to /home/laurent
-RUN_IDENTIFIER=$(date +%s)
-SERVER_TRANSFER_DIR=deployment_rsync/smbox_export_$RUN_IDENTIFIER
-TRANSFER_DIR=$SERVER_TRANSFER_DIR"_"$TAG_NAME
+SERVER_TRANSFER_DIR=deployment_smtoolbox
 
 
 echo
 echo "### Performing GIT export"
 echo "Exporting code base from git."
-cd $
-# TODO: add check if clone or pull needs to be done, and run accordingly
-# git clone --quiet $GIT_URL $EXPORT_DIR
-
-# TODO: check if export dir doesn't exist
 cd $EXPORT_DIR
+# TODO: add check if clone or pull needs to be done, and run accordingly
+# TODO: check if export dir doesn't exist
+# git clone --quiet $GIT_URL $EXPORT_DIR
+git checkout master
 git pull
 git checkout --quiet $TAG_NAME
 git submodule update --quiet --init --recursive
 echo
 echo "### Create a TAR with the code base"
+rm -rf $TAR_DIR
 mkdir $TAR_DIR
-tar cfz $LOCAL_TAR_FILE --exclude '.git*' -C $EXPORT_DIR .
+tar cfz $LOCAL_TAR_FILE --exclude '.git*' --exclude 'deployment.sh' --exclude 'vendors.py' --exclude 'bin/*' -C $EXPORT_DIR .
+git checkout master
 echo
 echo "### Rsyncing to target server ($ACTIVE_SERVER)"
-rsync -aH $TAR_DIR/ $ACTIVE_SERVER:/home/laurent/$TRANSFER_DIR/
+rsync -aH $TAR_DIR/ $ACTIVE_SERVER:/home/laurent/$SERVER_TRANSFER_DIR/
 rm $LOCAL_TAR_FILE
+echo "### finished"
 
